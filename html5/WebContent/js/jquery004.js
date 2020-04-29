10565ee618ddcc213022f35c26abedebba7e75b3
yn01 <yn01@LAPTOP-U8PQJ21L>
2020-04-29 16:28:56 +0900

    2020-04-29

--- html5/WebContent/js/jquery004.js
+++ html5/WebContent/js/jquery004.js
@@ -31,19 +31,22 @@ $(function() {
 		bannerAction = setInterval(move, 3000);
 	});
 	*/
-	let bannerAction = setInterval(move, 3000);
-	
+	let bannerAction = setInterval(move, 9000);
+	/*duration마다 실행*/
 	$("section:first").hover(function(){
 		clearInterval(bannerAction);
 	}, function(){
-		bannerAction = setInterval(move, 3000);
+		bannerAction = setInterval(move, 9000);
 	});
 	
 	function move() {
-		$(".container").stop(true).animate( {"left" : "-640px"}, 2000, function() {
+		console.log("move 실행");
+		/*duration 동안 움직인다*/
+		$(".container").stop(true).animate( {"left" : "-640px"}, 5000, function() {
 			$(".container > div:first").appendTo(".container");
 			$(".container").css("left", "0");
 		});
+		console.log("move 종료");
 	}
 	
 	$("button:first").click(function(){
@@ -52,13 +55,13 @@ $(function() {
 		$(".container").css("left","-640px");
 		$(".container").animate({"left":"0"}, 1000);
 		clearInterval(bannerAction);
-		bannerAction = setInterval(move, 3000);
+		bannerAction = setInterval(move, 9000);
 	});
 
 	$("button:last").click(function(){
 		if($(".container").queue().length > 0) return false;
 		move();
 		clearInterval(bannerAction);
-		bannerAction = setInterval(move, 3000);
+		bannerAction = setInterval(move, 9000);
 	});
 });
